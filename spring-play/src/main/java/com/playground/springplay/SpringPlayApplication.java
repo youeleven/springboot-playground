7dc45def2a61c112fc31a7745d311b7aa79200f6
Urvi Negi <unegi@vmware.com>
2023-12-14 20:25:51 +0530

    initial commit

---
+++ spring-play/src/main/java/com/playground/springplay/SpringPlayApplication.java
@@ -0,0 +1,13 @@
+package com.playground.springplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlayApplication.class, args);
+	}
+
+}
---
+++ spring-play/src/test/java/com/playground/springplay/SpringPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.springplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
